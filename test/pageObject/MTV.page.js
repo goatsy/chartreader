c9010c26e80ef495f68d5bf472d6a1dbed907492
mzehetmeier <martin.zehetmeier@mytoys.de>
2019-02-17 21:51:40 +0100

    Formatting

--- test/pageObject/MTV.page.js
+++ test/pageObject/MTV.page.js
@@ -1,26 +1,39 @@
 const fs = require('fs');
 
-class MTVPage  {
+class MTVPage {
 
-    get loadMoreButton() { return $('.loadMoreBtn'); }
-    get acceptAGBButton() { return $('.evidon-banner-acceptbutton'); }
-    get chartItemsList() { return $$('.chartItemsBox'); }
-    get newEntriesList() { return $$("//div[@class='variation'][contains(text(),'neu')]"); }
-    get chartsMainTitle() { return $(".chartMainTitle"); }
+    get loadMoreButton() {
+        return $('.loadMoreBtn');
+    }
 
+    get acceptAGBButton() {
+        return $('.evidon-banner-acceptbutton');
+    }
+
+    get chartItemsList() {
+        return $$('.chartItemsBox');
+    }
+
+    get newEntriesList() {
+        return $$("//div[@class='variation'][contains(text(),'neu')]");
+    }
+
+    get chartsMainTitle() {
+        return $(".chartMainTitle");
+    }
 
 
     open(url) {
         browser.url(url);
     }
 
-    acceptAGBs(){
-        if(this.acceptAGBButton.isDisplayed())
-        this.acceptAGBButton.click();
+    acceptAGBs() {
+        if (this.acceptAGBButton.isDisplayed())
+            this.acceptAGBButton.click();
     }
 
     expandChartList() {
-        for(let i=0; i<=3; i++) {
+        for (let i = 0; i <= 3; i++) {
             this.loadMoreButton.waitForDisplayed();
             this.loadMoreButton.click();
             browser.pause(1000);
@@ -34,14 +47,14 @@ class MTVPage  {
         this.open(url);
         this.acceptAGBs();
         this.expandChartList();
-        let logCharts="";
-        logCharts+=`taken from ${browser.config.baseUrl}${url}\nAnzahl Neueinsteiger: ${this.newEntriesList.length}\n${this.chartsMainTitle.getText()}\n`;
+        let logCharts = "";
+        logCharts += `taken from ${browser.config.baseUrl}${url}\nAnzahl Neueinsteiger: ${this.newEntriesList.length}\n${this.chartsMainTitle.getText()}\n\n`;
         this.newEntriesList.forEach(function (newEntry) {
             let parentElement = newEntry.$('..').$('..').$('..');
             let songName = parentElement.$('.videoTitle').getText();
             let artistName = parentElement.$('.regular-content').getText();
             let currentPos = parentElement.$('.currentPos').getText();
-            logCharts+=`${currentPos}: ${songName} - ${artistName} \n`;
+            logCharts += `${currentPos}: ${songName} - ${artistName} \n`;
         });
         fs.writeFileSync(`./output/${filename}.txt`, logCharts);
     }
--- test/specs/chartreader.spec.js
+++ test/specs/chartreader.spec.js
@@ -6,24 +6,28 @@ const URL = require('../../URL').CHART_URLS;
 
 describe('mtv', function () {
     it('should display single charts', function () {
+        this.retries(2);
         MTVpage.collectCharts(URL.single, "single");
     });
 });
 
 describe('mtv', function () {
     it('should display album charts', function () {
+        this.retries(2);
         MTVpage.collectCharts(URL.album, "album");
     });
 });
 
 describe('mtv', function () {
     it('should display single midweeks charts', function () {
+        this.retries(2);
         MTVpage.collectCharts(URL.single_midweeks, "single_midweeks");
     });
 });
 
 describe('mtv', function () {
     it('should display album midweeks charts', function () {
+        this.retries(2);
         MTVpage.collectCharts(URL.album_midweeks, "album_midweeks");
     });
 });
